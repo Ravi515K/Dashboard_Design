5ce719a467feb0c9172ae4ff8b88a8344c647934
Ravi515K <koliravi95@gmail.com>
2023-11-20 13:33:28 +0530

    implement menu drop down headless tailwind

--- src/components/SingleMember.jsx
+++ src/components/SingleMember.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { IoIosArrowForward } from 'react-icons/io'
+// import { IoIosArrowForward } from 'react-icons/io'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 import { singleMemberData } from '../Redux/Slices/AddMember/AddMember'
+import DropDown from './DropDown'
 function SingleMember({name , role, url,id}) {
   //  console.log(id)
  
@@ -15,7 +16,8 @@ function SingleMember({name , role, url,id}) {
             navigate('/detail')
     }
     return (
-        <div className='flex justify-between w-250 py-2' index={id}>
+        <div className='flex' index={id}>
+            <div className='flex justify-between w-250 py-2' onClick={()=>goToDetailPage(id)}>
             <div className=''>
                 <img className='w-30 h-30 rounded-full' src={url} alt="" />
             </div>
@@ -23,7 +25,8 @@ function SingleMember({name , role, url,id}) {
                 <h1 className='text-[14px] font-medium'>{name}</h1>
                 <p className='text-[9px] text-[#7C7B7C]'>{role}</p>
             </div>
-            <div  className=' mr-2 mt-1 cursor-pointer' onClick={()=>goToDetailPage(id)}>< IoIosArrowForward /></div>
+            </div>
+            <div  className=' mr-2 mt-2 cursor-pointer' ><DropDown /></div>
         </div>
     )
 }
